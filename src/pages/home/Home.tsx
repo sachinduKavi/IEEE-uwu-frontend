695e56f26b6a3c1f26d0b5f3084ef7360e24b2c4
Ishini Dewamiththa <ishinidewamiththa@gmail.com>
2025-06-07 22:23:14 +0530

    add about us component

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Slider from '../../components/Slider'
 import UpcomingEventSection from '../../components/UpcomingEventSection'
+import AboutUs from "../../components/AboutUs.tsx";
 
 export default function Home() {
   return (
     <div className='screen'>
         <Slider />
+        <AboutUs/>
         <UpcomingEventSection/>
     </div>
   )
