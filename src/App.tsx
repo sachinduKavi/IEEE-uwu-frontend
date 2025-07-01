cfdb506ea7deaafe6ad27cbd8ff0d068fad2c545
Sajan-Hirusha <141469578+Sajan-Hirusha@users.noreply.github.com>
2025-07-01 19:05:37 +0530

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import { BrowserRouter, Routes, Route } from 'react-router'
 import Home from './pages/home/Home'
 import Events from './components/Events'
-import OpenDay from "./pages/Openday/OpenDay.tsx";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <BrowserRouter>
