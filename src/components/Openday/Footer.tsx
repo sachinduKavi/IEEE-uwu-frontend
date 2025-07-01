fe6a4344a901c5a810ae74fbb41d3348b875d213
Sajan-Hirusha <141469578+Sajan-Hirusha@users.noreply.github.com>
2025-07-01 20:13:21 +0530

    Developing open day web

--- src/components/Openday/Footer.tsx
+++ src/components/Openday/Footer.tsx
@@ -2,7 +2,7 @@ import { Facebook, Instagram, Linkedin, Mail, MapPin } from "lucide-react";
 
 export default function Footer() {
     return (
-        <footer className="bg-gradient-to-r from-blue-900 to-blue-800 text-white py-12">
+        <footer className="bg-gradient-to-r  from-blue-950/90 to-blue-800/90  text-white py-12">
             <div className="container mx-auto px-4">
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
                     <div>
